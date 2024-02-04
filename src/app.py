1acd8804ebf3e56712686b05552c7af6d7fa9eac
Huajie <hz642@cornell.edu>
2024-02-04 00:12:25 -0500

    setting up gui and keybind

--- src/app.py
+++ src/app.py
@@ -18,3 +18,35 @@ folder_path = ""
 scale_factor = 1.0
 original_image = None
 canvas_image = None
+
+drag_data = {"x": 0, "y": 0, "item": None}
+
+
+root = tk.Tk()
+root.title("Image Viewer")
+
+
+canvas = tk.Canvas(root, width=root.winfo_screenwidth(),
+                   height=root.winfo_screenheight())
+canvas.pack()
+
+open_button = tk.Button(root, text="Open Folder", command=open_folder)
+open_button.place(relx=0.0, rely=0.0, anchor='nw')
+
+root.geometry("854x480")
+
+root.bind("<Right>", next_image)
+root.bind("r", next_image)
+
+root.bind("<Left>", previous_image)
+root.bind("e", previous_image)
+
+root.bind("<MouseWheel>", zoom)
+
+root.bind("<Button-1>", start_drag)
+root.bind("<B1-Motion>", do_drag)
+root.bind("<ButtonRelease-1>", stop_drag)
+
+root.bind('f', favorite_image)
+
+root.mainloop()
